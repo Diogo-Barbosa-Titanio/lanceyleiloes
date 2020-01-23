0b17c7d172c88af44f59e83e1ae5437c247fa8ea
Diogo Barbosa Silva Sousa <diogo.barbosa@gm5.com.br>
2020-01-23 17:06:47 -0300

    Ajustes no layout

--- resources/views/front/pessoa_fisica.blade.php
+++ resources/views/front/pessoa_fisica.blade.php
@@ -5,31 +5,31 @@
     <div class="container pessoa_fisica">
 
         <div class="row">
-            <div class="col-12 col-sm-12 mt-4">
+            <div class="col s12">
                 @include('alert_danger')
             </div>
         </div>
 
         <form action="{{url('/')}}/pessoa_fisica/store" method="post">
             @csrf
-            <div class="form-row">
-                <div class="col-sm-12">
+            <div class="row">
+                <div class="col s12 xl12">
                     <h2>
                         <span class="texto">Faça seu cadastro</span>
                     </h2>
                 </div>
             </div>
 
-            <div class="form-row botoes">
-                <div class="col-sm-3">
+            <div class="row botoes">
+                <div class="col s12 xl3">
                     <a href="{{url('/')}}/pessoa_fisica" class="btn btn-cor-principal">Pessoa Física</a>
 
                     <a href="{{url('/')}}/pessoa_juridica" class="btn btn-cor-auxiliar">Pessoa Jurídica</a>
                 </div>
             </div>
 
-            <div class="form-row">
-                <div class="form-group col-sm-6">
+            <div class="row">
+                <div class="input-field col s12 xl6">
                     <label for="nome">Nome*</label>
                     <input type="text" class="form-control @error('nome') is-invalid @enderror" value="{{ old('nome') }}" name="nome" id="nome" required placeholder="nome">
 
@@ -40,7 +40,7 @@
                     @enderror
                 </div>
 
-                <div class="form-group col-sm-6">
+                <div class="input-field col s12 xl6">
                     <label for="email">Email*</label>
                     <input type="email" class="form-control @error('email') is-invalid @enderror" value="{{ old('email') }}" name="email" id="email" required placeholder="email">
 
@@ -52,8 +52,8 @@
                 </div>
             </div>
 
-            <div class="form-row">
-                <div class="form-group col-sm-6">
+            <div class="row">
+                <div class="input-field col s12 xl6">
                     <label for="cpf">CPF*</label>
                     <input type="text" class="form-control @error('cpf') is-invalid @enderror" value="{{ old('cpf') }}" name="cpf" id="cpf" required placeholder="000.000.000-00">
 
@@ -64,7 +64,7 @@
                     @enderror
                 </div>
 
-                <div class="form-group col-sm-6">
+                <div class="input-field col s12 xl6">
                     <label for="rg">RG*</label>
                     <input type="text" class="form-control @error('rg') is-invalid @enderror" value="{{ old('rg') }}" name="rg" id="rg" required placeholder="00.000.000-0">
 
@@ -76,14 +76,15 @@
                 </div>
             </div>
 
-            <div class="form-row">
-                <div class="form-group col-sm-6">
-                    <label for="sexo">Sexo*</label>
+            <div class="row">
+                <div class="input-field col s12 xl6">
+
                     <select name="sexo" id="sexo" class="custom-select form-control @error('sexo') is-invalid @enderror">
-                        <option value="">Selecione</option>
+                        <option value="" disabled selected>Selecione</option>
                         <option value="0" {{'F'.old('sexo') == 'F0' ? 'selected' : ''}}>Feminino</option>
                         <option value="1" {{old('sexo') == 1 ? 'selected' : ''}}>Masculino</option>
                     </select>
+                    <label for="sexo">Sexo*</label>
 
                     @error('sexo')
                     <span class="invalid-feedback" role="alert">
@@ -92,7 +93,7 @@
                     @enderror
                 </div>
 
-                <div class="form-group col-sm-6">
+                <div class="input-field col s12 xl6">
                     <label for="nascimento">Data de Nascimento*</label>
                     <input type="date" class="form-control @error('nascimento') is-invalid @enderror" value="{{ old('nascimento') }}" name="nascimento" id="nascimento" required placeholder="00/00/0000">
 
@@ -104,8 +105,8 @@
                 </div>
             </div>
 
-            <div class="form-row">
-                <div class="form-group col-sm-6">
+            <div class="row">
+                <div class="input-field col s12 xl6">
                     <label for="telefone">Telefone*</label>
                     <input type="text" class="form-control  @error('telefone') is-invalid @enderror" value="{{ old('telefone') }}" name="telefone" id="telefone" placeholder="">
 
@@ -116,14 +117,14 @@
                     @enderror
                 </div>
 
-                <div class="form-group col-sm-6">
+                <div class="input-field col s12 xl6">
                     <label for="celular">Celular</label>
                     <input type="tel" class="form-control" value="{{ old('celular') }}" name="celular" id="celular" placeholder="">
                 </div>
             </div>
 
-            <div class="form-row">
-                <div class="form-group col-sm-2">
+            <div class="row">
+                <div class="input-field col s12 xl2">
                     <label for="cep">Cep*</label>
                     <input type="text" class="form-control @error('cep') is-invalid @enderror" value="{{ old('cep') }}" name="cep" id="cep" placeholder="">
 
@@ -135,46 +136,46 @@
 
                 </div>
 
-                <div class="form-group col-sm-6">
+                <div class="input-field col s12 xl6">
                     <label for="endereco">Endereço</label>
                     <input type="text" class="form-control" value="{{ old('endereco') }}" name="endereco" id="endereco" placeholder="">
                 </div>
 
-                <div class="form-group col-sm-2">
+                <div class="input col s12 xl2">
                     <label for="numero">Número</label>
                     <input type="number" class="form-control" value="{{ old('numero') }}" name="numero" id="numero" placeholder="">
                 </div>
 
-                <div class="form-group col-sm-2">
+                <div class="input col s12 xl2">
                     <label for="complemento">Complemento</label>
                     <input type="text" class="form-control" value="{{ old('complemento') }}" name="complemento" id="complemento" placeholder="">
                 </div>
             </div>
 
-            <div class="form-row">
-                <div class="form-group col-sm-3">
+            <div class="row">
+                <div class="input-field col s12 xl3">
                     <label for="bairro">Bairro</label>
                     <input type="text" class="form-control" value="{{ old('bairro') }}" name="bairro" id="bairro" placeholder="">
                 </div>
 
-                <div class="form-group col-sm-3">
+                <div class="input-field col s12 xl3">
                     <label for="cidade">Cidade</label>
                     <input type="text" class="form-control" value="{{ old('cidade') }}" name="cidade" id="cidade" placeholder="">
                 </div>
 
-                <div class="form-group col-sm-3">
+                <div class="input-field col s12 xl3">
                     <label for="estado">Estado</label>
                     <input type="text" class="form-control" value="{{ old('estado') }}" name="estado" id="estado" placeholder="">
                 </div>
 
-                <div class="form-group col-sm-3">
+                <div class="input-field col s12 xl3">
                     <label for="pais">País</label>
                     <input type="text" class="form-control" value="{{ old('pais') }}" name="pais" id="pais" placeholder="">
                 </div>
             </div>
 
-            <div class="form-row">
-                <div class="form-group col-sm-4">
+            <div class="row">
+                <div class="input-field col s12 xl4">
                     <label for="login">Login*</label>
                     <input type="text" class="form-control  @error('login') is-invalid @enderror" value="{{ old('login') }}" name="login" id="login" placeholder="">
 
@@ -186,27 +187,27 @@
 
                 </div>
 
-                <div class="form-group col-sm-4">
+                <div class="input-field col s12 xl4">
                     <label for="senha">Senha</label>
                     <input type="password" class="form-control" name="senha" id="senha" placeholder="">
                 </div>
 
-                <div class="form-group col-sm-4">
+                <div class="input-field col s12 xl4">
                     <label for="confirma_senha">Confirma Senha</label>
                     <input type="password" class="form-control" name="confirma_senha" id="confirma_senha"
                            placeholder="">
                 </div>
             </div>
 
-            <div class="form-row">
-                <div class="form-group col-sm-10">
-                    <input class="" type="checkbox" id="termo_de_uso" name="termo_de_uso">
+            <div class="row">
+                <div class="input-field col s12 xl10">
                     <label for="termo_de_uso">
-                        Eu aceito os Termos de Uso
+                       <input class="" type="checkbox" id="termo_de_uso" name="termo_de_uso">
+                       <span>Eu aceito os Termos de Uso</span>
                     </label>
                 </div>
 
-                <div class="form-group col-sm-2 text-right">
+                <div class="input-field col s12 xl2">
                     <button class="btn btn-cor-principal">Cadastrar</button>
                 </div>
             </div>
--- resources/views/front/pessoa_juridica.blade.php
+++ resources/views/front/pessoa_juridica.blade.php
@@ -5,31 +5,31 @@
     <div class="container pessoa_juridica">
 
         <div class="row">
-            <div class="col-12 col-sm-12 mt-4">
+            <div class="col s12 xl12">
                 @include('alert_danger')
             </div>
         </div>
 
         <form action="{{url('/')}}/pessoa_juridica/store" method="post">
             @csrf
-            <div class="form-row">
-                <div class="col-sm-12">
+            <div class="row">
+                <div class="col s12 xl12">
                     <h2>
                         <span class="texto">Faça seu cadastro</span>
                     </h2>
                 </div>
             </div>
 
-            <div class="form-row botoes">
-                <div class="col-sm-3">
+            <div class="row botoes">
+                <div class="col s12 xl3">
                     <a href="{{url('/')}}/pessoa_fisica" class="btn btn-cor-auxiliar">Pessoa Física</a>
 
                     <a href="{{url('/')}}/pessoa_juridica" class="btn btn-cor-principal">Pessoa Jurídica</a>
                 </div>
             </div>
 
-            <div class="form-row">
-                <div class="form-group col-sm-6">
+            <div class="row">
+                <div class="input-field col s12 xl6">
                     <label for="razao_social">Razão Social*</label>
                     <input type="text" class="form-control @error('razao_social') is-invalid @enderror" value="{{ old('razao_social') }}" name="razao_social" id="razao_social" placeholder="">
                     @error('razao_social')
@@ -39,7 +39,7 @@
                     @enderror
                 </div>
 
-                <div class="form-group col-sm-6">
+                <div class="input-field col s12 xl6">
                     <label for="email">Email*</label>
                     <input type="email" class="form-control @error('email') is-invalid @enderror" value="{{ old('email') }}" name="email" id="email" placeholder="">
                     @error('email')
@@ -50,8 +50,8 @@
                 </div>
             </div>
 
-            <div class="form-row">
-                <div class="form-group col-sm-6">
+            <div class="row">
+                <div class="input-field col s12 xl6">
                     <label for="cnpj">CNPJ*</label>
                     <input type="text" class="form-control @error('cnpj') is-invalid @enderror" value="{{ old('cnpj') }}" name="cnpj" id="cnpj" placeholder="">
                     @error('cnpj')
@@ -61,7 +61,7 @@
                     @enderror
                 </div>
 
-                <div class="form-group col-sm-6">
+                <div class="input-field col s12 xl6">
                     <label for="ie">Inscrição Estadual*</label>
                     <input type="text" class="form-control @error('ie') is-invalid @enderror" value="{{ old('ie') }}" name="ie" id="ie" placeholder="">
                     @error('ie')
@@ -72,8 +72,8 @@
                 </div>
             </div>
 
-            <div class="form-row">
-                <div class="form-group col-sm-6">
+            <div class="row">
+                <div class="input-field col s12 xl6">
                     <label for="telefone">Telefone*</label>
                     <input type="text" class="form-control @error('telefone') is-invalid @enderror" value="{{ old('telefone') }}" name="telefone" id="telefone" placeholder="">
                     @error('ie')
@@ -83,14 +83,14 @@
                     @enderror
                 </div>
 
-                <div class="form-group col-sm-6">
+                <div class="input-field col s12 xl6">
                     <label for="celular">Celular</label>
                     <input type="tel" class="form-control" value="{{old('celular')}}" name="celular" id="celular" placeholder="">
                 </div>
             </div>
 
-            <div class="form-row">
-                <div class="form-group col-sm-2">
+            <div class="row">
+                <div class="input-field col s12 xl2">
                     <label for="cep">Cep*</label>
                     <input type="text" class="form-control @error('cep') is-invalid @enderror" value="{{ old('cep') }}" name="cep" id="cep" placeholder="">
                     @error('cep')
@@ -100,70 +100,70 @@
                     @enderror
                 </div>
 
-                <div class="form-group col-sm-6">
+                <div class="input-field col s12 xl6">
                     <label for="endereco">Endereço</label>
                     <input type="text" class="form-control" value="{{ old('endereco') }}" name="endereco" id="endereco" placeholder="">
                 </div>
 
-                <div class="form-group col-sm-2">
+                <div class="input-field col s12 xl2">
                     <label for="numero">Número</label>
                     <input type="number" class="form-control" value="{{ old('numero') }}" name="numero" id="numero" placeholder="">
                 </div>
 
-                <div class="form-group col-sm-2">
+                <div class="input-field col s12 xl2">
                     <label for="complemento">Complemento</label>
                     <input type="text" class="form-control" value="{{ old('complemento') }}" name="complemento" id="complemento" placeholder="">
                 </div>
             </div>
 
-            <div class="form-row">
-                <div class="form-group col-sm-3">
+            <div class="row">
+                <div class="input-field col s12 xl3">
                     <label for="bairro">Bairro</label>
                     <input type="text" class="form-control" value="{{ old('bairro') }}" name="bairro" id="bairro" placeholder="">
                 </div>
 
-                <div class="form-group col-sm-3">
+                <div class="input-field col s12 xl3">
                     <label for="cidade">Cidade</label>
                     <input type="text" class="form-control" value="{{ old('cidade') }}" name="cidade" id="cidade" placeholder="">
                 </div>
 
-                <div class="form-group col-sm-3">
+                <div class="input-field col s12 xl3">
                     <label for="estado">Estado</label>
                     <input type="text" class="form-control" value="{{ old('estado') }}" name="estado" id="estado" placeholder="">
                 </div>
 
-                <div class="form-group col-sm-3">
+                <div class="input-field col s12 xl3">
                     <label for="pais">País</label>
                     <input type="text" class="form-control" value="{{ old('pais') }}" name="pais" id="pais" placeholder="">
                 </div>
             </div>
 
-            <div class="form-row">
-                <div class="form-group col-sm-4">
+            <div class="row">
+                <div class="input-field col s12 xl4">
                     <label for="login">Login*</label>
                     <input type="text" class="form-control" value="{{ old('login') }}" name="login" id="login" placeholder="">
                 </div>
 
-                <div class="form-group col-sm-4">
+                <div class="input-field col s12 xl4">
                     <label for="senha">Senha</label>
                     <input type="password" class="form-control" name="senha" id="senha" placeholder="">
                 </div>
 
-                <div class="form-group col-sm-4">
+                <div class="input-field col s12 xl4">
                     <label for="confirma_senha">Confirma Senha</label>
                     <input type="password" class="form-control" name="confirma_senha" id="confirma_senha" placeholder="">
                 </div>
             </div>
 
-            <div class="form-row">
-                <div class="form-group col-sm-10">
-                    <input class="" type="checkbox" id="termo_de_uso" name="termo_de_uso">
+            <div class="row">
+                <div class="input-field col s12 xl10">
                     <label for="termo_de_uso">
-                        Eu aceito os Termos de Uso
+                       <input class="" type="checkbox" id="termo_de_uso" name="termo_de_uso">
+                       <span>Eu aceito os Termos de Uso</span>
                     </label>
                 </div>
 
-                <div class="form-group col-sm-2 text-right">
+                <div class="input-field col s12 xl2">
                     <button class="btn btn-cor-principal">Cadastrar</button>
                 </div>
             </div>
